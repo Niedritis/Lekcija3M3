acc28c1c07f6a5b85d7bd0714f28b15e3a4117dd
Niedritis <niedritis@inbox.lv>
2022-04-24 21:22:39 +0300

    ggg

---
+++ M3/M3/Prece.cs
@@ -0,0 +1,16 @@
+﻿namespace M3
+{
+    public class Prece
+    {
+        public Product(double height, double width, double weight)
+        {
+            Height = h;
+            Width = w;
+            Weight = m;
+        }
+        public double Height { get; }
+        public double Width { get; }
+        public double Weight { get; }
+
+    }
+}
